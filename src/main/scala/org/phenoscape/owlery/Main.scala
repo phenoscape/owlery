29d4cd83d0f0395e78cf50b86d55a3559755d639
Jim Balhoff <balhoff@nescent.org>
2014-05-15 15:53:49 -0500

    Practicing route configuration.

--- src/main/scala/org/phenoscape/owlery/Main.scala
+++ src/main/scala/org/phenoscape/owlery/Main.scala
@@ -5,15 +5,42 @@ import akka.actor.ActorSystem
 
 object Main extends App with SimpleRoutingApp {
 
-  implicit val system = ActorSystem("my-system")
+  implicit val system = ActorSystem("owlery-system")
 
   startServer(interface = "localhost", port = 8080) {
-    path("hello") {
-      get {
-        complete {
-          <h1>Say hello to spray</h1>
+    pathPrefix("kb" / Segment) { kbName =>
+      parameters('object, 'prefixes.?, 'direct ? true) { (owlObject, prefixes, direct) =>
+        path("subclasses") {
+          complete {
+            kbName + s": $direct subclasses of " + owlObject
+          }
+        } ~
+          path("superclasses") {
+            complete {
+              kbName + s": $direct superclasses of " + owlObject
+            }
+          } ~
+          path("equivalent") {
+            complete {
+              kbName + ": equivalent to " + owlObject
+            }
+          } ~
+          path("satisfiable") {
+            complete {
+              kbName + ": " + owlObject + " satisfiable?"
+            }
+          } ~
+          path("types") {
+            complete {
+              kbName + s": $direct types for " + owlObject
+            }
+          }
+      } ~
+        pathEnd {
+          complete {
+            kbName
+          }
         }
-      }
     }
   }
 
