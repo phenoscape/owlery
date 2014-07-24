efd7afe2c109e7ba34ffa357f824a01665e71618
Jim Balhoff <balhoff@nescent.org>
2014-07-24 13:42:41 -0400

    SPARQL queries now working.

--- src/main/scala/org/phenoscape/owlery/Knowledgebase.scala
+++ src/main/scala/org/phenoscape/owlery/Knowledgebase.scala
@@ -2,15 +2,14 @@ package org.phenoscape.owlery
 
 import org.phenoscape.owlet.Owlet
 import org.semanticweb.owlapi.reasoner.OWLReasoner
-
 import com.hp.hpl.jena.query.Query
+import com.hp.hpl.jena.query.ResultSet
 
 case class Knowledgebase(name: String, reasoner: OWLReasoner) {
 
-  def performSPARQLQuery(query: Query): String = {
+  def performSPARQLQuery(query: Query): ResultSet = {
     val owlet = new Owlet(this.reasoner)
-    //TODO
-    ???
+    owlet.performSPARQLQuery(query)
   }
 
 }
