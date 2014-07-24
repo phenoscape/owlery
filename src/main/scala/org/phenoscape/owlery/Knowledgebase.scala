84ecb7f38c049acbc69faee1d9c813549fbf3e6c
Jim Balhoff <balhoff@nescent.org>
2014-07-23 23:33:07 -0400

    Upgraded Owlet to 1.1.1

--- src/main/scala/org/phenoscape/owlery/Knowledgebase.scala
+++ src/main/scala/org/phenoscape/owlery/Knowledgebase.scala
@@ -1,15 +1,14 @@
 package org.phenoscape.owlery
 
+import org.phenoscape.owlet.Owlet
 import org.semanticweb.owlapi.reasoner.OWLReasoner
+
 import com.hp.hpl.jena.query.Query
-import com.hp.hpl.jena.query.QueryExecutionFactory
-import com.hp.hpl.jena.rdf.model.ModelFactory
-import org.phenoscape.owlet.QueryExpander
 
 case class Knowledgebase(name: String, reasoner: OWLReasoner) {
 
   def performSPARQLQuery(query: Query): String = {
-    val owlet = new QueryExpander(this.reasoner)
+    val owlet = new Owlet(this.reasoner)
     //TODO
     ???
   }
