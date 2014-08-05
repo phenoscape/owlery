e31e25ccf779fcb3c9c39c4b7261bfe2543ed88e
Jim Balhoff <balhoff@nescent.org>
2014-08-05 16:16:01 -0400

    Added detach to avoid blocking on reasoner queries.

--- src/main/scala/org/phenoscape/owlery/Main.scala
+++ src/main/scala/org/phenoscape/owlery/Main.scala
@@ -44,59 +44,75 @@ object Main extends App with SimpleRoutingApp {
         case Some(kb) => {
           path("subclasses") {
             parameters('object.as[IRI], 'prefixes.?, 'direct ? true) { (owlObject, prefixes, direct) =>
-              complete {
-                val subclasses = kb.reasoner.getSubClasses(factory.getOWLClass(owlObject), direct).getFlattened
-                subclasses.map(_.getIRI.toString).mkString("\n")
+              detach() {
+                complete {
+                  val subclasses = kb.reasoner.getSubClasses(factory.getOWLClass(owlObject), direct).getFlattened
+                  subclasses.map(_.getIRI.toString).mkString("\n")
+                }
               }
             }
           } ~
             path("superclasses") {
               parameters('object.as[IRI], 'prefixes.?, 'direct ? true) { (owlObject, prefixes, direct) =>
-                complete {
-                  val superclasses = kb.reasoner.getSuperClasses(factory.getOWLClass(owlObject), direct).getFlattened
-                  superclasses.map(_.getIRI.toString).mkString("\n")
+                detach() {
+                  complete {
+                    val superclasses = kb.reasoner.getSuperClasses(factory.getOWLClass(owlObject), direct).getFlattened
+                    superclasses.map(_.getIRI.toString).mkString("\n")
+                  }
                 }
               }
             } ~
             path("equivalent") {
               parameters('object.as[IRI], 'prefixes.?) { (owlObject, prefixes) =>
-                complete {
-                  val equivalents = kb.reasoner.getEquivalentClasses(factory.getOWLClass(owlObject)).getEntities
-                  equivalents.map(_.getIRI.toString).mkString("\n")
+                detach() {
+                  complete {
+                    val equivalents = kb.reasoner.getEquivalentClasses(factory.getOWLClass(owlObject)).getEntities
+                    equivalents.map(_.getIRI.toString).mkString("\n")
+                  }
                 }
               }
             } ~
             path("satisfiable") {
               parameters('object.as[IRI], 'prefixes.?) { (owlObject, prefixes) =>
-                complete {
-                  kb.reasoner.isSatisfiable(factory.getOWLClass(owlObject)).toString
+                detach() {
+                  complete {
+                    kb.reasoner.isSatisfiable(factory.getOWLClass(owlObject)).toString
+                  }
                 }
               }
             } ~
             path("types") {
               parameters('object.as[IRI], 'prefixes.?, 'direct ? true) { (owlObject, prefixes, direct) =>
-                complete {
-                  val types = kb.reasoner.getTypes(factory.getOWLNamedIndividual(owlObject), direct).getFlattened
-                  types.map(_.getIRI.toString).mkString("\n")
+                detach() {
+                  complete {
+                    val types = kb.reasoner.getTypes(factory.getOWLNamedIndividual(owlObject), direct).getFlattened
+                    types.map(_.getIRI.toString).mkString("\n")
+                  }
                 }
               }
             } ~
             path("sparql") {
               get {
                 parameter('query.as[Query]) { query =>
-                  complete {
-                    kb.performSPARQLQuery(query)
+                  detach() {
+                    complete {
+                      kb.performSPARQLQuery(query)
+                    }
                   }
                 }
               } ~
                 post {
-                  handleWith(kb.performSPARQLQuery)
+                  detach() {
+                    handleWith(kb.performSPARQLQuery)
+                  }
                 }
             } ~
             pathEnd {
-              complete {
-                val consistent = if (kb.reasoner.isConsistent) "consistent" else "inconsistent"
-                s"Knowledgebase $kbName is $consistent"
+              detach() {
+                complete {
+                  val consistent = if (kb.reasoner.isConsistent) "consistent" else "inconsistent"
+                  s"Knowledgebase $kbName is $consistent"
+                }
               }
             }
         }
