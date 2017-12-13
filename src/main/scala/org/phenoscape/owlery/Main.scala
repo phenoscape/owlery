37bbf403df34b9a33eff40346a2519bf82f5b711
Jim Balhoff <jim@balhoff.org>
2017-12-13 15:52:08 -0500

    Make hostname configurable.

--- src/main/scala/org/phenoscape/owlery/Main.scala
+++ src/main/scala/org/phenoscape/owlery/Main.scala
@@ -80,8 +80,9 @@ object Main extends App with SimpleRoutingApp with CORSDirectives {
 
   val conf = ConfigFactory.load()
   val serverPort = conf.getInt("owlery.port")
+  val host = conf.getString("owlery.host")
 
-  startServer(interface = "localhost", port = serverPort) {
+  startServer(interface = host, port = serverPort) {
 
     corsFilter(List("*")) {
       pathPrefix("kbs") {
