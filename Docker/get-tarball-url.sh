854251b255216616ec94e547ad1e8c82ae5f2faa
Hilmar Lapp <hlapp@drycafe.net>
2017-12-14 15:03:19 -0500

    Parameterizes some strings for easier reuse

--- Docker/get-tarball-url.sh
+++ Docker/get-tarball-url.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 TARGET=$1
+REPO=$2
 
 if [ -z $TARGET ] ; then
-    echo $(curl -s https://api.github.com/repos/phenoscape/owlery/releases/latest | grep tarball_url | cut -d\" -f4)
+    echo $(curl -s https://api.github.com/repos/$REPO/releases/latest | grep tarball_url | cut -d\" -f4)
 else
-    echo "https://api.github.com/repos/phenoscape/owlery/tarball/$TARGET"
+    echo "https://api.github.com/repos/$REPO/tarball/$TARGET"
 fi
